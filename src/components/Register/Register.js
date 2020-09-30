0b7cf6eaa7c02ae947dc5391ab969311a36e1dd0
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-09-30 16:45:38 +0200

    Modifications for deployment

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -21,7 +21,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3001/register", {
+    fetch("https://shrouded-reaches-95938.herokuapp.com/register", {
       method: "post",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3001/signin", {
+    fetch("https://shrouded-reaches-95938.herokuapp.com/signin", {
       method: "post",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({
