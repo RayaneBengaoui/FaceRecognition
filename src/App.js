5b65f6ce318aefec7b66dc4cf4eae1def4576138
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-09-30 14:32:11 +0200

    Removing API key from front-end + adding verification on user ID for forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
-import Clarifai from "clarifai";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
@@ -10,10 +9,6 @@ import Register from "./components/Register/Register";
 
 import "./App.css";
 import Particles from "react-particles-js";
-
-const app = new Clarifai.App({
-  apiKey: "72296d9f724f4f768e9df7151ec3a9bf",
-});
 
 const particlesOptions = {
   particles: {
@@ -111,8 +106,14 @@ class App extends React.Component {
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    app.models
-      .predict(Clarifai.DEMOGRAPHICS_MODEL, this.state.input)
+    fetch("http://localhost:3001/imageurl", {
+      method: "post",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
       .then((response) => {
         if (response) {
           fetch("http://localhost:3001/image", {
